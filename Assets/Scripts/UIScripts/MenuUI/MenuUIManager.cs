3464ea0cab56a1e107da56349b0abea7b9df4135
231028 <c231028@ivy.ac.jp>
2025-10-30 16:37:01 +0900

    UIManager編集

--- Assets/Scripts/UIScripts/MenuUI/MenuUIManager.cs
+++ Assets/Scripts/UIScripts/MenuUI/MenuUIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8aca4baba54e0d7e1449faeb707f107e662f7c8e02bed95b805e70361a4089bc
-size 804
+oid sha256:4016b70a9889e455a7c0074841af97f6b0c5617475bffe0f9fb25081605e236b
+size 806
