18dd51fb2c574c97f3c898010a473eb761b8c21c
231028 <c231028@ivy.ac.jp>
2025-10-31 10:12:37 +0900

    シーン遷移でCanvasが消えないスクリプト追加

---
+++ Assets/Scripts/UIScripts/CanvasTakeover.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a710eb1b015eb6d7a95c33d1ea45680f65cdb0b5ccec560ca548b20add976121
+size 462
--- Assets/Scripts/UIScripts/MenuUI/PosePanelButton.cs
+++ Assets/Scripts/UIScripts/MenuUI/PosePanelButton.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d9f1be7f1daf8d2ece5705f5a413213eb26ee2ad1bbad9112923198900f6e06
-size 1237
+oid sha256:7446dd3c6dec870a10ce189dd6b88341fe50c88fb0435a81feb05f0241d283a6
+size 1018
