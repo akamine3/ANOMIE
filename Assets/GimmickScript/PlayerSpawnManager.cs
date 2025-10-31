b4867229f1d51d9966c7d0a481f72b0fdff0b5d3
AkamineSadaharu <c231001@ivy.ac.jp>
2025-10-31 14:29:53 +0900

    シーン遷移時のプレイヤー出現地点リセット

--- Assets/GimmickScript/PlayerSpawnManager.cs
+++ Assets/GimmickScript/PlayerSpawnManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4f2ad2ae47eeee6b8d38dcb0f854c5d2c9ecc41e86e36b4d45501566a2b55f0
-size 730
+oid sha256:b0ce75062008bb32739be1b86360521aee4b09744dfa7c33bf2d22149cb7d4d8
+size 732
