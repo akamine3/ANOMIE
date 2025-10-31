816eb32fa5a3eafc1f14238f212e275cb3120e0c
AkamineSadaharu <c231001@ivy.ac.jp>
2025-10-31 14:25:04 +0900

    プレイヤーをMap1のから持ち越すように変更

--- Assets/GimmickScript/CameraFllow.cs
+++ Assets/GimmickScript/CameraFllow.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9bc3b62a9a9e570eb67de5c6808ffcd248553283c4783036baaec359bf5d62cc
-size 346
+oid sha256:6dc9eda7c44ea420f072c17d0d810f36e7003f0b1f4218d9aaa62796a31c9220
+size 1002
---
+++ Assets/GimmickScript/PlayerSpawnManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a4f2ad2ae47eeee6b8d38dcb0f854c5d2c9ecc41e86e36b4d45501566a2b55f0
+size 730
