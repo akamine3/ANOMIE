ab458345e03f67f2f9f5d8f0e49bcb70e0b39b66
231009-azukimausu <152138039+231009-azukimausu@users.noreply.github.com>
2025-10-31 16:12:53 +0900

    ダイアログ関係スクリプト

---
+++ Assets/DialogueScript/DialogueTrigger.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b6e43e351d40db23b0c1e06ce3511d62c5880d3fed90fda39e8539d1fcc0e392
+size 2818
---
+++ Assets/DialogueScript/NPCDialogueTrigger.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:98d0add6bb070e952309eac5f1e4c39870043729e8b31769e7857f306803ae9b
+size 3244
