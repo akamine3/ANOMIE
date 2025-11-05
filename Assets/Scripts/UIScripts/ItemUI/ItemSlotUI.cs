a4c8956527dcc3b64d725188d247b9e6f993d829
231028 <c231028@ivy.ac.jp>
2025-11-05 10:29:35 +0900

    アイテムUIのバグ修正

--- Assets/Scripts/UIScripts/ItemUI/ItemSlotUI.cs
+++ Assets/Scripts/UIScripts/ItemUI/ItemSlotUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b917af330cef0a0d7ba19a60e3f42bf20fac86e6e09531201c0b28944003bb3
-size 2071
+oid sha256:a5d27dac1e26d6d7e064fcc2a30ea534f9373973610bba1c9d01ec029a6425fa
+size 2112
--- Assets/Scripts/UIScripts/ItemUI/ItemUIManager.cs
+++ Assets/Scripts/UIScripts/ItemUI/ItemUIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90c164bcc4bbca72c0af45431b9ac70ecc4fe5b4a9eabff56181125f68803508
-size 8975
+oid sha256:10e8501728beb3a93630c0ea7dd17d1f47c724d3873af8907cbd16dcd16ff4de
+size 10835
