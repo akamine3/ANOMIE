9aabd80c69d6a41f4e21dd6b6bfb8425aba6f1bd
231028 <c231028@ivy.ac.jp>
2025-11-06 10:56:19 +0900

    UIの配置修正

---
+++ Assets/Scripts/AudioScripts/SettingPanelManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a931bd95d70880625f0ac3ff7af43ffd4d6a08b5e9d56779553e77a524d9f072
+size 812
--- Assets/Scripts/UIScripts/MenuUI/MenuUIManager.cs
+++ Assets/Scripts/UIScripts/MenuUI/MenuUIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9091e3b5b68b464c351b3e87d5b3d02b9426f9002e56aedbf87d1da6c2e6055
-size 1495
+oid sha256:3f7b92ebc169dec9955936801c44a18ba68115c950fdf8b313221936996d564b
+size 1338
