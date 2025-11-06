24fada09dd2c1a41a65985ac5697eba4bef3e959
231028 <c231028@ivy.ac.jp>
2025-11-06 14:23:10 +0900

    音量調節キャンバスの設定完了

--- Assets/Scripts/UIScripts/MenuUI/MenuUIManager.cs
+++ Assets/Scripts/UIScripts/MenuUI/MenuUIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f7b92ebc169dec9955936801c44a18ba68115c950fdf8b313221936996d564b
-size 1338
+oid sha256:2972b86064efae8c11d52e346de482cbddf98bb84f2079e13ef851341245b4e6
+size 1431
--- Assets/Scripts/UIScripts/MenuUI/PosePanelButton.cs
+++ Assets/Scripts/UIScripts/MenuUI/PosePanelButton.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b2c67dbcfb353aab58e7b2f244fb42b6d911e7bc94d127e9a3208dec120a64d
-size 1008
+oid sha256:aad639ed01d78514150a688ae451602760136e8ef3f398ebf2e6ec3bce14208d
+size 1202
