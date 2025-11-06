76c1ea70a59b0e3912a8795aef3634177104a1bb
231028 <c231028@ivy.ac.jp>
2025-11-06 15:34:54 +0900

    NoItemが表示されないバグを修正

--- Assets/Scripts/UIScripts/ItemUI/ItemUIManager.cs
+++ Assets/Scripts/UIScripts/ItemUI/ItemUIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5cbbec0ef5d70e440207bc90841f7bfdfaa989b21a1b9f798534b650f0d7efaf
-size 10832
+oid sha256:8a934627047e08458e82eeb023a58f3b47090e1964407340e42a6f7c83beb770
+size 10326
