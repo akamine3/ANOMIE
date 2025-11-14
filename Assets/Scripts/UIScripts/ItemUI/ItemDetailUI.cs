ec4d41a1f52c4084114dc389d8fca3b40eb34b6b
231028 <c231028@ivy.ac.jp>
2025-11-14 11:33:22 +0900

    MenuUIのフォント変更・日本語化

--- Assets/Scripts/UIScripts/ItemUI/ItemDetailUI.cs
+++ Assets/Scripts/UIScripts/ItemUI/ItemDetailUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09eeb269301cfbb7be20a8b5986b85323bfa1ff15f979232462b6881c6465017
-size 4599
+oid sha256:ac4ffef89b7a76b2669547da31db762662cad24ae4a8e29b41dc682b16d6e980
+size 4612
