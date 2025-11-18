5c40440b7970526f379def2b0d047491ae5b0cd1
AkamineSadaharu <c231001@ivy.ac.jp>
2025-11-18 11:50:44 +0900

    commit

--- Assets/Scripts/UIScripts/MenuUI/InventoryManager.cs
+++ Assets/Scripts/UIScripts/MenuUI/InventoryManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:585e8b4d4902c2db8a53c22dead528ea7532998f79cc999a6fbb63fb53a49b29
-size 1736
+oid sha256:a1d294059e6cb19fdf08604f8ff55c0e6512972515342f7cc4dbfcbbc4a4d887
+size 1734
