4361afaa890aadd2666acac527b67445beb3f0d7
AkamineSadaharu <c231001@ivy.ac.jp>
2025-11-19 16:34:41 +0900

    Map1で団長と会話可能に

--- Assets/DialogueKankei/Kari/MovePlayerScript.cs
+++ Assets/DialogueKankei/Kari/MovePlayerScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1cd2c708be9455cd67adb8ce0d9fbe55410db2072ff0e4571c252384f8492ef
-size 1244
+oid sha256:6caf4f2412d8c1e51bdedf13ecbd32b9b087e6a494e31aa415dd459f2cb39afc
+size 1755
