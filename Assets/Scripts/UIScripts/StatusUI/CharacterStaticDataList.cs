dd051ce03b9d0f38242ebc9cf734943405fe1f65
231028 <c231028@ivy.ac.jp>
2025-11-21 16:11:37 +0900

    PlayerStatusUIの作成

--- Assets/Scripts/UIScripts/StatusUI/CharacterStaticDataList.cs
+++ Assets/Scripts/UIScripts/StatusUI/CharacterStaticDataList.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1da0a898007e6beca844b31fa12d56d154b554fb222296379b3993199f02949f
-size 938
+oid sha256:4e047becbe2b085fa8282d3dca9e29e9512da9b1ffd82cab1375dba1586779ee
+size 1100
