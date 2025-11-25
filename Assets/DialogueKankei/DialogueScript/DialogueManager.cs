d09baf54e65315853ff48806ec85782aca54b63a
231009-azukimausu <152138039+231009-azukimausu@users.noreply.github.com>
2025-11-25 09:50:30 +0900

    ひとまず移動のスクリプト(移動にディレイが発生するので調整予定)

--- Assets/DialogueKankei/DialogueScript/DialogueManager.cs
+++ Assets/DialogueKankei/DialogueScript/DialogueManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f854ccb45ca2cd4c326340cf4f5e9cceb02cb8fc3e85635f1e20550f1d8716ba
-size 26685
+oid sha256:fd225b97b963f9286e081214204e5e09408f536ba975008da5b6ea1a3070021c
+size 30371
---
+++ Assets/DialogueKankei/DialogueScript/MoveManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:03224bc6da05ca971cee1b3d1e16fe451a7e9c7b36140da3921040334d7cb540
+size 2825
