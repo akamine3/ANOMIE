7e4be58483a0b0f052b93d0fa86192f24777fae1
231028 <c231028@ivy.ac.jp>
2025-11-27 14:59:03 +0900

    ステータス画面完成

--- Assets/Scripts/UIScripts/StatusUI/PlayerStatusUI.cs
+++ Assets/Scripts/UIScripts/StatusUI/PlayerStatusUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cae5dd7ce1cd6d17c77daee08373270fb0189392372673b4c4fff25051fa8e7f
-size 4099
+oid sha256:f16ab9f7a17147cf8f14a1dd008c8151303cb5cd3f26e3f5a0e63c5cbcfe0f5c
+size 4215
--- Assets/Scripts/UIScripts/StatusUI/StatusUIManager.cs
+++ Assets/Scripts/UIScripts/StatusUI/StatusUIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8f916eee369ceb7f7a71f0a00db8ace1a7a4bc64315f6c0fff35a060081b46f
-size 5430
+oid sha256:377b8f25247139a4b458ea45ebef14a89bc087d8c68b707860ff428323bcb045
+size 6139
