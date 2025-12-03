1b26b80a9e7bbd2114e5aa6c91ca8000e5d3c9f4
231028 <c231028@ivy.ac.jp>
2025-12-03 13:14:12 +0900

    UIの一部変更

---
+++ Assets/Scripts/AudioScripts/AudioManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4d7387bf71d19e607115452e3a4748d3d6768fbcaedf108c19fd1c71596debd7
+size 452
--- Assets/Scripts/UIScripts/ItemUI/ItemSlotUI.cs
+++ Assets/Scripts/UIScripts/ItemUI/ItemSlotUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5d27dac1e26d6d7e064fcc2a30ea534f9373973610bba1c9d01ec029a6425fa
-size 2112
+oid sha256:0ff3d28edb6cf45a14c178558a1f6819c307095bf5646fe10455e554c0a536bc
+size 2332
