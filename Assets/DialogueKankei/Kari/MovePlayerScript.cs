daa4bbde0c674aaa706401e69013aa507f10e2ef
AkamineSadaharu <c231001@ivy.ac.jp>
2025-12-04 15:37:40 +0900

    左右移動時の反転追加

--- Assets/DialogueKankei/Kari/MovePlayerScript.cs
+++ Assets/DialogueKankei/Kari/MovePlayerScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ac98dd1b90ad024dc0b883a41684d7e8aa192800e79ee6ee5a41428cee84996
-size 1915
+oid sha256:9aaf7079fc2fdf54d13f84076f68200bd1d6b0ae6c6f6a1179c0d46c814910ad
+size 2395
