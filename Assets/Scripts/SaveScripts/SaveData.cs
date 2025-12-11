16a88a08937be42365ec066fbbfc16dd7b0bd393
231028 <c231028@ivy.ac.jp>
2025-12-11 15:47:25 +0900

    セーブ機能実装用ファイル作成

---
+++ Assets/Scripts/SaveScripts/SaveData.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:40e412e2e41f64792f35d46c7ad065086f4279f9ee00b04775b97e8b3e062fca
+size 230
---
+++ Assets/Scripts/SaveScripts/SaveManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2cb857e73b8b583908e51be37409e79a3eb963e3aef4c6ad22c69222c554d494
+size 313
