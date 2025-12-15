2c51ec0c8f6f4fc9105924f2f70ee31f003bb47c
231028 <c231028@ivy.ac.jp>
2025-12-15 13:09:52 +0900

    オートセーブスロットに上書き保存できないようにする処理を追加

--- Assets/Scripts/SaveScripts/SaveManager.cs
+++ Assets/Scripts/SaveScripts/SaveManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cff01b9265724bc1441696951c619cfc484be49a77f6c3b5040d72adfa01b912
-size 2381
+oid sha256:af9ff9ca8252966c01a9d10a04630d57ed9b2f84002d78d67c421699fee835b0
+size 2508
--- Assets/Scripts/UIScripts/ItemUI/PlayerInventory.cs
+++ Assets/Scripts/UIScripts/ItemUI/PlayerInventory.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15bdc33c6069f52250bc79678b63a86d1ba20e307ab09dd11ab04aa107e8274b
-size 5294
+oid sha256:dd0f1c0007fde76d99519a71bcce85980a46be42fc668bc6c468ed3ad4a6681f
+size 5305
