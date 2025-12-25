3679172d5e224ea932907251608de6f28f308220
AkamineSadaharu <c231001@ivy.ac.jp>
2025-12-26 01:36:50 +0900

    commit

--- Assets/DialogueKankei/DialogueScript/DialogueManager.cs
+++ Assets/DialogueKankei/DialogueScript/DialogueManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04236b91b2ab0d1fd0bcba47beb8df1a43bc4d861291316a6bbe6c547465aa2b
-size 37992
+oid sha256:74b38e8e374e3cb2224753ad5bcacae472e50acf717f2b9f794729c6d74936ab
+size 38670
--- Assets/GimmickScript/SceneChange.cs
+++ Assets/GimmickScript/SceneChange.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea97df01bc17c318f5ee7116d1f302a139e15af9698cb9416a5debbca4b1d374
-size 808
+oid sha256:f993d882abadc9fc6e00cf3f0301240399c1ba5d7b98ab38a0e70b392911c711
+size 1144
