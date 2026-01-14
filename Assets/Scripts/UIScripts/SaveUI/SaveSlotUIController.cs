ff0a9d1fb384d7af54b0d80a0a9c4fc0838b1693
231028 <c231028@ivy.ac.jp>
2026-01-14 12:01:22 +0900

    no message

--- Assets/Scripts/UIScripts/SaveUI/SaveSlotUIController.cs
+++ Assets/Scripts/UIScripts/SaveUI/SaveSlotUIController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6d7783ca3975ef5b04f04743e22b85f63fc43103ed05c9c51e25a1eaf1e7b3a
-size 1031
+oid sha256:d1345b530a30e4ecbde7c36882734710bc3f1c13ed9189b57c6b84016ba4121b
+size 1014
