e68ccf031acbba54b8941a09884cb282d0177335
231028 <c231028@ivy.ac.jp>
2026-01-19 13:37:15 +0900

    OverlayMessageButton.cs エラー部分をコメントアウト

--- Assets/DialogueKankei/DialogueScript/Readable Data Script/OverlayMessageButton.cs
+++ Assets/DialogueKankei/DialogueScript/Readable Data Script/OverlayMessageButton.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:829c19e6d0cdade31cd037c7513732254bdeb5b394fca4fee7b70f576307d207
-size 508
+oid sha256:09b404bb94eb011ba4ad4204f632cc38caa6e6878279072ee333f8d1f7eb34d1
+size 510
