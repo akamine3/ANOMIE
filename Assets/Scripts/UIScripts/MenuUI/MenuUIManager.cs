c5909a1f7d05975143342e31db6804df438cd5c5
231028 <c231028@ivy.ac.jp>
2026-01-20 14:44:07 +0900

    タイトル画面の作成・アイテムデータベースの修正

--- Assets/Scripts/UIScripts/MenuUI/MenuUIManager.cs
+++ Assets/Scripts/UIScripts/MenuUI/MenuUIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8acadabcb30ca59ff3e97e85b5ad5ef39280a6c9af05590aedae6e310ab63df0
-size 6906
+oid sha256:18a1dca02ae63dd70f2229fc091170316479d7374c09e297117dd2da6e54cc65
+size 6966
