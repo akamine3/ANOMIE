cb90bea8b450a88cbc35b00c4f0d6a9a303526d2
231028 <c231028@ivy.ac.jp>
2026-01-20 11:35:35 +0900

    エラー修正

--- Assets/Scripts/UIScripts/MenuUI/InventoryManager.cs
+++ Assets/Scripts/UIScripts/MenuUI/InventoryManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9c9295e5391235d308cef5f37ec61fde614c490d473c89b32551e0851a56aae
-size 1715
+oid sha256:86d93e3a0017b381ab1f81835f6927bc76cfc441834f978a3d76cb2ea0fe249c
+size 1773
