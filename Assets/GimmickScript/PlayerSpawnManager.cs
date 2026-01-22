1dd140191e343218e83dd5965483788f345adc3a
231028 <c231028@ivy.ac.jp>
2026-01-22 11:26:21 +0900

    ロードした時SAN値が反映されないバグを解消

--- Assets/GimmickScript/PlayerSpawnManager.cs
+++ Assets/GimmickScript/PlayerSpawnManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33087ff443daf130965f48504d1389d22bb35200dfc1dd40cd3b00ab0afcec56
-size 1058
+oid sha256:e1a5a493c8b9d18efdb6f37a8fd32f8ceb9ca5daee27a27ae3868ecd1aeddc00
+size 1056
