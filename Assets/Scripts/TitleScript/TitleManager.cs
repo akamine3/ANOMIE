4c131e5e96db19e83482eaa0271b8006ffde5f23
231028 <c231028@ivy.ac.jp>
2026-01-23 13:43:44 +0900

    はじめからボタンを押したときにmap1に移動する機能を作成

--- Assets/Scripts/TitleScript/TitleManager.cs
+++ Assets/Scripts/TitleScript/TitleManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12798f3bf7e55778c51bb8d483731d0dddbae565dc7658a90221849b6c63122f
-size 314
+oid sha256:02c6155a1da63fdae0bd73ca25d5c671238ef47c6d3a8622e981559ac12d2133
+size 1273
