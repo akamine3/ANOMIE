6f62124bbeb6fae9c7b3fc5e1bf9356ae042a074
231028 <c231028@ivy.ac.jp>
2026-01-23 10:27:57 +0900

    ロード時のバグ解消

--- Assets/GimmickScript/PlayerSpawnManager.cs
+++ Assets/GimmickScript/PlayerSpawnManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f223d08d3f06de60f1ba090362c09528523816ae98c4fd220a22e19153002b42
-size 6090
+oid sha256:fadb78c6630165d571a366a6f149c913df999bbe9362a7d67d80ec14edac7087
+size 4461
--- Assets/Scripts/SaveScripts/LoadGameController.cs
+++ Assets/Scripts/SaveScripts/LoadGameController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37ccee726d4f488da29b33b94a8294771fcdca8403c6194eb1c687d1ec797505
-size 1477
+oid sha256:4de4af76c484cacc646c0123545721697eac4b3fef586bb711ead8004a792954
+size 1034
