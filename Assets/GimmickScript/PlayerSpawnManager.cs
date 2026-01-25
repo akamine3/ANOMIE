44ca8109172284b6e166616d98472da26cd34d33
231028 <c231028@ivy.ac.jp>
2026-01-26 01:24:06 +0900

    ワープポイントがずれるバグ修正

--- Assets/GimmickScript/PlayerSpawnManager.cs
+++ Assets/GimmickScript/PlayerSpawnManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11dc12193d770d860a8f7bf0063daaedb5288ed4a8ddfa9b068cd56fb7295401
-size 4216
+oid sha256:5c8d97c24bc84d5cae8cd6d96286bc46358a82d38eeda8dd868d05ca8d322688
+size 4699
