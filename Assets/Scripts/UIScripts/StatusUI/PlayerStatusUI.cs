4e41352b01aad678361664047e84c573acc99aba
231028 <c231028@ivy.ac.jp>
2026-01-26 14:23:27 +0900

    善悪ポイントがステータス画面の性格に反映されるように修正

--- Assets/Scripts/UIScripts/StatusUI/PlayerStatusUI.cs
+++ Assets/Scripts/UIScripts/StatusUI/PlayerStatusUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44cccfee3693f59bf72ce490bf037e557f61ad4a4c69d0b18a5433787b3490b7
-size 4219
+oid sha256:5504eb1c0c9c91c106bde132c3e737cf1b0db8df0c8b4d5d4210094544fa72e3
+size 5994
